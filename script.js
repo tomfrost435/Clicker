4da75a6abbdaeda4372753f0c116a35239b08199
Tommaso Acciarresi <49035039+tomfrost435@users.noreply.github.com>
2024-08-06 03:56:55 +0200

    modified:   clicker.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-let num = 0;
+let currency = 0;
+let cps = 0;
+const upgrades = {
+  cursor: { power: 1, count: 0 },
+  grandma: { power: 5, count: 0 },
+  farm: { power: 10, count: 0 },
+};
 
-function increaseNumber() {
-  num++;
-  document.getElementById("numberDisplay").innerText = num;
+function increaseCurrency() {
+  currency++;
+  document.getElementById("numberDisplay").innerText = Math.round(currency);
 }
 
-function buyUpgrade() {}
+function buyUpgrade(upgradeId) {
+  const upgrade = upgrades[upgradeId];
+  if (upgrade) {
+    upgrade.count++;
+    cps += upgrade.power;
+    document.getElementById(`${upgradeId}Count`).innerText = upgrade.count;
+  }
+}
+
+function updateCurrency() {
+  currency += cps;
+  document.getElementById("numberDisplay").innerText = Math.round(currency);
+}
+
+setInterval(updateCurrency, 1000);
